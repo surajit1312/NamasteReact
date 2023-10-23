4bc8b4fe327422410405d48b210366857e25a450
surajit1312 <surajit1602@gmail.com>
2023-10-23 23:51:23 +0530

    feat[render-react]: Add React and bundler 'parcel' to hot-deploy the application

---
+++ p2_igniting_react_app/App.js
@@ -0,0 +1,54 @@
+/**
+ * Parcel Benefits
+ * - Creation of Live Server
+ * - (HMR) Hot Module Replacement
+ * - File watcher algorithm (C++)
+ * - BUNDLING
+ * - Code Minification
+ * - Code cleanup
+ * - Local and Production build
+ * - Super Fast Build algorithm
+ * - Image Optimization
+ * - Caching while development
+ * - Code Compression
+ * - Compatibility with old version of browsers
+ * - HTTPS on Dev
+ * - Port creation
+ * - Consistent Hashing Algorithm
+ * - Zero Configuration
+ * - Transitive Dependency resolutions
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+    className: "title",
+    key: "heading-1",
+  },
+  "Namaste everyone from React!"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+    className: "title",
+    key: "heading-2",
+  },
+  "This is an illustration of creating elements from React!"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    className: "container",
+  },
+  [heading1, heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
